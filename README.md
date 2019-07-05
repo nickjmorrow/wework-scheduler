27d36fb236cbe20462a0b684e1ffa2d30e9d4975
Nicholas Morrow <njmorrow95@gmail.com>
2019-07-05 12:19:32 -0400

    Readme

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-Professional portfolio.
-
-TODO: Actual README ;)
+TODO
--- client/getServerUrl.js
+++ client/getServerUrl.js
@@ -1,15 +1,12 @@
 const getServerUrl = () => {
 	switch (process.env.NODE_ENV) {
-		case "production":
-			return "https://agile-reaches-55352.herokuapp.com/";
-		case "development":
-			return "http://localhost:9000/";
+		case 'development':
+			return 'https://fierce-sierra-91208.herokuapp.com/';
+		case 'x':
+			return 'http://localhost:9000/';
 		default:
 			throw new Error(`Unexpected environment: ${process.env.NODE_ENV}`);
 	}
-	
-}
-// const getServerUrl = () => process.env.NOD_ENV === "development" ? "localhost:3000" : "https://agile-reaches-55352.herokuapp.com/"
+};
 
-// const getServerUrl = () => "http://localhost:3000";
 module.exports = getServerUrl;
